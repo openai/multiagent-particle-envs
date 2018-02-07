507653412650998c80d3e0c9acc5227f5e3f0029
Janet Deng <janetjied@gmail.com>
2018-02-07 17:48:26 +0100

    Initially create actor and critic models.

---
+++ maddpg/models.py
@@ -0,0 +1,43 @@
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Actor(nn.Module):
+    def __init__(self, dim_observation, dim_action):
+        super(Actor, self).__init__()
+        self.FC1 = nn.Linear(dim_observation, 64)
+        self.FC2 = nn.Linear(64, 64)
+        self.FC3 = nn.Linear(64, dim_action)
+
+    def forward(self, obs):
+        result = F.relu(self.FC1(obs))
+        result = F.relu(self.FC2(result))
+        result = F.softmax(self.FC3(result))
+        return result
+
+
+class Critic(nn.Module):
+    def __init__(self, n_agent, dim_observation, dim_action):
+        super(Critic, self).__init__()
+        obs_dim = dim_observation * n_agent
+        act_dim = dim_action * n_agent
+
+        self.FC1 = nn.Linear(obs_dim, 64)       # nn.Linear(obs_dim+act_dim, 64)
+        self.FC2 = nn.Linear(64+act_dim, 64)
+        self.FC3 = nn.Linear(64, 1)
+
+    def forward(self, obs, acts):
+        result = F.relu(self.FC1(obs))
+        combined = th.cat([result, acts], 1)    # concatenate tensors in columns
+        result = F.relu(self.FC2(combined))
+        result = self.FC3(result)
+        return result
+
+
+
+
+
+
+
+
---
+++ maddpg/multi_agent.py
@@ -0,0 +1,33 @@
+from make_env import make_env
+import numpy as np
+import time
+
+env = make_env('simple_speaker_listener')
+
+for i_episode in range(1):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        agent_actions = []
+        for i, agent in enumerate(env.world.agents):
+            agent_action_space = env.action_space[i]
+            action = agent_action_space.sample()
+            action_vec = np.zeros(agent_action_space.n)
+            action_vec[action] = 1
+            agent_actions.append(action_vec)
+
+        time.sleep(0.033)
+        observation, reward, done, info = env.step(agent_actions)
+
+        print(observation)
+        print(reward)
+        print(done)
+        print(info)
+        print()
+
+
+
+
+
+
+
