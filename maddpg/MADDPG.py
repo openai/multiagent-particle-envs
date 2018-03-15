Correct last commit, no noise added, but using Gumbel-softmax instead of softmax, better result so far.

maddpg/MADDPG.py
@@ -217,12 +217,12 @@ class MADDPG:
             act = self.actors[i](sb)
             # print(act)
 
-            '''
+
             # ? to add exploration rate here ?
             if self.isOU:   # TODO
                 act += Variable(th.from_numpy(self.ou_noises[i]() * self.var[i]).type(FloatTensor))
-            else:
-                act += Variable(th.from_numpy(np.random.randn(self.dim_act_list[i]) * self.var[i]).type(FloatTensor))
+            # else:
+                # act += Variable(th.from_numpy(np.random.randn(self.dim_act_list[i]) * self.var[i]).type(FloatTensor))
                 # print('act', act)
 
             # use more exploration??
@@ -230,8 +230,6 @@ class MADDPG:
                 self.var[i] *= 0.999998
 
             act = th.clamp(act, -1.0, 1.0)
-            '''
-
             actions[:, index_act:(index_act+self.dim_act_list[i])] = act
             # print('actions', actions)
 

maddpg/models.py
@@ -9,14 +9,19 @@ nodes = 64
 
 
 def normal_noise(dim_action):
-    n = np.random.uniform(size=dim_action)
-    n = -np.log(-np.log(n))
-    return Variable(th.from_numpy(n).type(th.cuda.FloatTensor))
-
-
-def uniform_noise(dim_action):
     n = np.random.randn(dim_action)
     return Variable(th.from_numpy(n).type(th.cuda.FloatTensor))
+
+
+def gumbel_sample(dim_action, eps=1e-20):
+    n = np.random.uniform(size=dim_action)
+    n = -np.log(-np.log(n + eps) + eps)
+    return Variable(th.from_numpy(n).type(th.cuda.FloatTensor))
+
+
+def gumbel_softmax(ret, dim_action, temp=1):
+    ret = ret + gumbel_sample(dim_action)
+    return F.softmax(ret/temp)
 
 
 class Actor(nn.Module):
@@ -31,8 +36,7 @@ class Actor(nn.Module):
         result = F.relu(self.FC1(obs))
         result = F.relu(self.FC2(result))
         result = self.FC3(result)
-        result = result + uniform_noise(self.dim_action)  # normal_noise(self.dim_action)
-        result = F.softmax(result)
+        result = gumbel_softmax(result, self.dim_action)
         return result
 
 