bd12a438ae017ce17d87cf080a952b441e24e77e
linlinbest <444053358@qq.com>
2019-07-20 17:23:23 -0400

    Modified policy.py so that agents can go to the landmark automatically. No learning algorithms implemented yet.

--- multiagent/policy.py
+++ multiagent/policy.py
@@ -1,6 +1,8 @@
 import numpy as np
 from pyglet.window import key
 
+from multiagent.scenarios.simple import Scenario
+
 # individual agent policy
 class Policy(object):
     def __init__(self):
@@ -14,6 +16,7 @@ class InteractivePolicy(Policy):
     def __init__(self, env, agent_index):
         super(InteractivePolicy, self).__init__()
         self.env = env
+        #self.agent_index = agent_index
         # hard-coded keyboard events
         self.move = [False for i in range(4)]
         self.comm = [False for i in range(env.world.dim_c)]
@@ -23,6 +26,28 @@ class InteractivePolicy(Policy):
 
     def action(self, obs):
         # ignore observation and just act based on keyboard events
+
+        
+        #x_axis = self.env.agents[self.agent_index].state.p_pos[0]
+        #y_axis = self.env.agents[self.agent_index].state.p_pos[1]
+
+        if obs[2] < 0:
+            self.move[1] = True
+        elif obs[2] > 0:
+            self.move[0] = True
+        else:
+            self.move[0] = False
+            self.move[1] = False
+
+        if obs[3] > 0:
+            self.move[3] = True
+        elif obs[3] < 0:
+            self.move[2] = True
+        else:
+            self.move[2] = False
+            self.move[3] = False
+        
+
         if self.env.discrete_action_input:
             u = 0
             if self.move[0]: u = 1
@@ -31,12 +56,12 @@ class InteractivePolicy(Policy):
             if self.move[3]: u = 3
         else:
             u = np.zeros(5) # 5-d because of no-move action
-            if self.move[0]: u[1] += 1.0
-            if self.move[1]: u[2] += 1.0
-            if self.move[3]: u[3] += 1.0
-            if self.move[2]: u[4] += 1.0
+            if self.move[0]: u[1] += 0.01
+            if self.move[1]: u[2] += 0.01
+            if self.move[3]: u[3] += 0.01
+            if self.move[2]: u[4] += 0.01
             if True not in self.move:
-                u[0] += 1.0
+                u[0] += 0.01
         return np.concatenate([u, np.zeros(self.env.world.dim_c)])
 
     # keyboard event callbacks
