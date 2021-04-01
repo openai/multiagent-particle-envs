6148ddbcb5fd5c3ed8ec741ac2dc3c2354f852f9
Kiran Prasad <kiranprasad@hotmail.com>
2021-03-31 23:49:19 -0700

    Noisy environment update
    Added wrapper around env that creates noisy observations.
    PartialObsMAE in environment.py
    Example output in bin/interactive.py
    Gaussian, exponential or uniform noise in the range [0,1) can be
    added to each observation.
    History of each agent is also tracked and can be truncated or
    left to grow infinitely (may run into OOM problems)
    Added seed toggle for reproducibility

--- bin/interactive.py
+++ bin/interactive.py
@@ -3,7 +3,7 @@ import os,sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import argparse
 
-from multiagent.environment import MultiAgentEnv
+from multiagent.environment import MultiAgentEnv, PartialObsMAE
 from multiagent.policy import InteractivePolicy
 import multiagent.scenarios as scenarios
 
@@ -18,22 +18,23 @@ if __name__ == '__main__':
     # create world
     world = scenario.make_world()
     # create multiagent environment
-    env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, info_callback=None, shared_viewer = False)
+    # env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, info_callback=None, shared_viewer = False)
+    env = PartialObsMAE(world, scenario.reset_world, scenario.reward, scenario.observation, info_callback=None, shared_viewer = False)
     # render call to create viewer window (necessary only for interactive policies)
     env.render()
     # create interactive policies for each agent
     policies = [InteractivePolicy(env,i) for i in range(env.n)]
     # execution loop
-    obs_n = env.reset()
+    obs_n, hist_n = env.reset()
     while True:
         # query for action from each agent's policy
         act_n = []
         for i, policy in enumerate(policies):
             act_n.append(policy.action(obs_n[i]))
         # step environment
-        obs_n, reward_n, done_n, _ = env.step(act_n)
+        obs_n, reward_n, done_n, _, hist_n = env.step(act_n)
         # render all agent views
         env.render()
         # display rewards
-        #for agent in env.world.agents:
-        #    print(agent.name + " reward: %0.3f" % env._get_reward(agent))
+        for agent in env.world.agents:
+           print(agent.name + " reward: %0.3f" % env._get_reward(agent))
