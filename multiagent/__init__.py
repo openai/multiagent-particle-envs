47713952b7faabd5a26fbd69d80b8472f144d22d
Jordan Terry <jkterry0@gmail.com>
2022-12-29 21:49:35 -0500

    Update __init__.py

--- multiagent/__init__.py
+++ multiagent/__init__.py
@@ -1,4 +1,5 @@
 from gym.envs.registration import register
+import warnings
 
 # Multiagent envs
 # ----------------------------------------
@@ -16,3 +17,9 @@ register(
     entry_point='multiagent.envs:SimpleSpeakerListenerEnv',
     max_episode_steps=100,
 )
+
+warnings.warn("This code base is no longer maintained, and is not expected to be maintained again in the future. \n' \
+'For the past handful of years, these environments been maintained inside of PettingZoo (see https://pettingzoo.farama.org/environments/mpe/ \n).' \
+'This maintained version includes documentation, support for the PettingZoo API, support for current versions of Python, numerous bug fixes, \n'' \
+'support for installation via pip, and numerous other large quality of life improvements. We encourage researchers to consider switching to this \n' \
+'version for any purposes other than comparing to results run on this version of the environments.'
